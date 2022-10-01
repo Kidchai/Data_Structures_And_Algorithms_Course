73f76326eec63dfa833ca125cbb3d3ee281ac108
Kidchai <o.naibova@gmail.com>
2022-10-01 18:21:11 -0500

    Implement deque on array

---
+++ src/main/java/kidchai/algortithms/tasks/DequeUsingArray/DequeArray.java
@@ -0,0 +1,38 @@
+package kidchai.algortithms.tasks.DEQusingArray;
+
+public class DequeArray {
+    int[] deque = new int[10];
+    int startIndex = 0;
+    int endIndex = 1;
+    int elementCounter = 0;
+
+    public int[] pushFront(int element) {
+        deque[startIndex] = element;
+        startIndex = startIndex == 0 ? deque.length - 1 : --startIndex;
+        elementCounter++;
+        return deque;
+    }
+
+    public int[] pushBack(int element) {
+        deque[endIndex] = element;
+        endIndex = endIndex == deque.length - 1 ? 0 : ++endIndex;
+        elementCounter++;
+        return deque;
+    }
+
+    public int popFront() {
+        int result = deque[startIndex + 1];
+        startIndex++;
+        elementCounter--;
+        return result;
+    }
+
+    public int popBack() {
+        int result = deque[endIndex - 1];
+        startIndex++;
+        elementCounter--;
+        return result;
+    }
+}
+
+
