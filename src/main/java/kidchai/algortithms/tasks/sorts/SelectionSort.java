09d0e6c19cb31cb130b9beaed3cf3abb3617b023
Kidchai <o.naibova@gmail.com>
2022-10-20 18:25:08 -0500

    Refactor Selection sort

--- src/main/java/kidchai/algortithms/tasks/sorts/SelectionSort.java
+++ src/main/java/kidchai/algortithms/tasks/sorts/SelectionSort.java
@@ -3,16 +3,15 @@ package kidchai.algortithms.tasks.sorts;
 public class SelectionSort {
     public static void sort(int[] array) {
         for (int i = 0; i < array.length; i++) {
-            int min = Integer.MAX_VALUE;
-            int indexOfMin = 0;
+            int minIndex = i;
             for (int j = i; j < array.length; j++) {
-                if (min > array[j]) {
-                    min = array[j];
-                    indexOfMin = j;
+                if (array[minIndex] > array[j]) {
+                    minIndex = j;
                 }
             }
-            array[indexOfMin] = array[i];
-            array[i] = min;
+            int buf = array[minIndex];
+            array[minIndex] = array[i];
+            array[i] = buf;
         }
     }
 }
